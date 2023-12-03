165c00c82a20ed353519df9f08b6f235f4976b19
SamBobBarnes <samuelrbarnes@outlook.com>
2023-12-03 01:06:31 -0600

    day 3 part 1

--- AOC/src/Day3/Part1.java
+++ AOC/src/Day3/Part1.java
@@ -2,10 +2,109 @@ package Day3;
 
 import Base.AdventBase;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
 public class Part1 extends AdventBase {
-    public static int Run() {
+    public static int Run(boolean example) {
         Start(3,1);
 
-        return 0;
+        List<String> input = LoadInput(3,example);
+
+        String symbolRegex = "[^0-9.]+";
+        String numberRegex = "[0-9]+";
+
+        List<Number> numbers = new ArrayList<>();
+        List<Symbol> symbols = new ArrayList<>();
+
+        int yIndex = 0;
+        for(String line: input) {
+            symbols.addAll(FindSymbols(line, yIndex));
+            numbers.addAll(FindNumbers(line,yIndex));
+
+            yIndex++;
+        }
+
+        int result = 0;
+        for(Number number: numbers) {
+            if(IsPartNumber(number,symbols)) result += number.value;
+        }
+
+
+        return result;
     }
+
+    private static boolean IsPartNumber(Number number, List<Symbol> symbols) {
+        int rangeX1 = number.x-1;
+        int rangeX2 = number.x+ number.length;
+        int rangeY1 = number.y-1;
+        int rangeY2 = number.y+1;
+
+        return symbols.stream().anyMatch(s -> s.x >= rangeX1 && s.x <= rangeX2 && s.y >= rangeY1 && s.y <= rangeY2);
+    }
+
+    private static List<Symbol> FindSymbols(String line, int yIndex) {
+        List<Symbol> symbols = new ArrayList<>();
+
+        char[] chars = line.toCharArray();
+
+        int xIndex = 0;
+        for(char character: chars) {
+            if((character < 48 || character > 57) && (character != 46)) {
+                symbols.add(new Symbol(xIndex,yIndex));
+            }
+            xIndex++;
+        }
+
+        return symbols;
+    }
+
+    private static List<Number> FindNumbers(String line, int yIndex) {
+        List<Number> numbers = new ArrayList<>();
+
+        char[] chars = line.toCharArray();
+
+        StringBuilder word = new StringBuilder();
+
+        int xIndex = 0;
+        for(char character: chars) {
+            if(character >= 48 && character <= 57) {
+                word.append(character);
+            }
+            else if(!word.isEmpty()) {
+                int length = word.length();
+                numbers.add(new Number(xIndex-length,yIndex,parseInt(word.toString()),length));
+                word = new StringBuilder();
+            }
+            xIndex++;
+        }
+
+        if(!word.isEmpty()) {
+            int length = word.length();
+            numbers.add(new Number(xIndex-length,yIndex,parseInt(word.toString()),length));
+        }
+
+        return numbers;
+    }
+}
+
+class Number extends Symbol {
+    public Number(int x, int y, int value, int length) {
+        super(x,y);
+        this.value = value;
+        this.length = length;
+    }
+    public int value;
+    public int length;
+}
+
+class Symbol {
+    public Symbol(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int x;
+    public int y;
 }
--- AOC/tests/MainRunnerTests.java
+++ AOC/tests/MainRunnerTests.java
@@ -1,7 +1,18 @@
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class MainRunnerTests {
+    @DataPoints("example")
+    public static boolean[] example() {
+        return new boolean[]{true,false};
+    }
+
     //region Day1
     @Test
     public void Day1Part1() {
@@ -35,15 +46,15 @@ public class MainRunnerTests {
     }
     //endregion
     //region Day3
-    @Test
-    public void Day3Part1() {
+    @Theory
+    public void Day3Part1(@FromDataPoints("example") boolean example) {
 
-        int actual = Day3.Part1.Run();
+        int actual = Day3.Part1.Run(example);
 
-        Assertions.assertEquals(0, actual);
+        Assertions.assertEquals(example ? 4361:544664, actual);
     }
-    @Test
-    public void Day3Part2() {
+    @Theory
+    public void Day3Part2(@FromDataPoints("example") boolean example) {
 
         int actual = Day3.Part2.Run();
 
