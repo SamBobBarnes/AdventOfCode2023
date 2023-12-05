0a8d0c5edab5461abd39852fa4975dfbd9c37d4e
SamBobBarnes <samuelrbarnes@outlook.com>
2023-12-04 23:45:47 -0600

    day5 part1 wip

--- AOC/src/Day5/Part1.java
+++ AOC/src/Day5/Part1.java
@@ -1,7 +1,11 @@
 package Day5;
 
 import Base.AdventBase;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 public class Part1 extends AdventBase {
     public static int Run(boolean example) {
@@ -10,5 +14,43 @@ public class Part1 extends AdventBase {
         List<String> input = LoadInput(5, example);
 
         return 0;
+    }
+
+    private static ConversionRange GetRanges(List<String> conversionTable) {
+        var source = conversionTable.get(0).split(" ")[0].split("-")[0];
+        var destination = conversionTable.get(0).split(" ")[0].split("-")[2];
+
+        var range = new ConversionRange(source, destination);
+
+        conversionTable.remove(0);
+
+        for(var line: conversionTable) {
+            var nums = line.split(" ");
+            range.AddConversion(parseInt(nums[1]),parseInt(nums[0]),parseInt(nums[2]));
+        }
+
+
+        return range;
+    }
+}
+
+class ConversionRange {
+    public ConversionRange(String sourceType, String destinationType) {
+        this.sourceType = sourceType;
+        this.destinationType = destinationType;
+        this.source = new ArrayList<>();
+        this.destination = new ArrayList<>();
+        this.range = new ArrayList<>();
+    }
+    public List<Integer> source;
+    public List<Integer> destination;
+    public List<Integer> range;
+    public String destinationType;
+    public String sourceType;
+
+    public void AddConversion(int source, int destination, int range) {
+        this.source.add(source);
+        this.destination.add(destination);
+        this.range.add(range);
     }
 }
