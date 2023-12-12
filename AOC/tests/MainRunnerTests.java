45cb94d82dc25b505fe9795734b4ded129a0b9bc
sb17057 <sbarnes@vermeer.com>
2023-12-12 16:58:52 -0600

    day 11 part 1

--- AOC/tests/MainRunnerTests.java
+++ AOC/tests/MainRunnerTests.java
@@ -185,7 +185,7 @@ public class MainRunnerTests {
 
         var actual = Day11.Part1.Run(example);
 
-        Assertions.assertEquals(example ? 0:0, actual);
+        Assertions.assertEquals(example ? 374:0, actual);
     }
     @Theory
     public void Day11Part2(@FromDataPoints("example") boolean example) {
