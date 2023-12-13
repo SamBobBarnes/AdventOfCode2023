d46a2ccc18a074ad37332bad87b8b1e864a70832
sb17057 <sbarnes@vermeer.com>
2023-12-13 17:03:57 -0600

    day 13 wip

--- AOC/src/Day13/Part1.java
+++ AOC/src/Day13/Part1.java
@@ -1,6 +1,8 @@
 package Day13;
 
 import Base.AdventBase;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Part1 extends AdventBase {
@@ -8,7 +10,41 @@ public class Part1 extends AdventBase {
         Start(13, 1, example);
 
         List<String> input = LoadInput(13, example);
+        var patterns = ProcessInput(input);
 
         return 0;
     }
+
+    private static List<Pattern> ProcessInput(List<String> list) {
+        var patterns = new ArrayList<Pattern>();
+        var temp = new ArrayList<String>();
+
+        for(var line: list) {
+            if(line.isEmpty()) {
+                patterns.add(new Pattern(temp));
+                temp = new ArrayList<>();
+            }
+            else {
+                temp.add(line);
+            }
+        }
+
+        patterns.add(new Pattern(temp));
+
+        return patterns;
+    }
+}
+
+class Pattern {
+    public Pattern(List<String> list) {
+        this.height = list.size();
+        this.width = list.getFirst().length();
+        this.pattern = new ArrayList<>();
+        for(var line: list) {
+            this.pattern.add(line.toCharArray());
+        }
+    }
+    public List<char[]> pattern;
+    public int width;
+    public int height;
 }
