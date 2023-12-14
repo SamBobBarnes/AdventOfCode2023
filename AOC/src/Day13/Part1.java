c07405ce93f694dcbf6477aeebafbf47087f79db
SamBobBarnes <samuelrbarnes@outlook.com>
2023-12-14 13:11:09 -0600

    day 13 wip

--- AOC/src/Day13/Part1.java
+++ AOC/src/Day13/Part1.java
@@ -12,7 +12,16 @@ public class Part1 extends AdventBase {
         List<String> input = LoadInput(13, example);
         var patterns = ProcessInput(input);
 
-        return 0;
+        var linesLeftOfMirror = 0;
+        var linesAboveMirror = 0;
+
+        for(var pattern: patterns) {
+            var lines = FindMirror(pattern);
+            if(lines > 0) linesLeftOfMirror += lines;
+            else linesAboveMirror -= lines;
+        }
+
+        return linesLeftOfMirror + (100 * linesAboveMirror);
     }
 
     private static List<Pattern> ProcessInput(List<String> list) {
@@ -32,6 +41,31 @@ public class Part1 extends AdventBase {
         patterns.add(new Pattern(temp));
 
         return patterns;
+    }
+
+    private static int FindMirror(Pattern pattern) {
+        var lines = FindHorizontalMirror(pattern);
+        if(lines >= 0) return -lines;
+        return FindVerticalMirror(pattern);
+    }
+
+    private static int FindVerticalMirror(Pattern pattern) {
+        return 0;
+    }
+
+    private static int FindHorizontalMirror(Pattern pattern) {
+        var length = pattern.pattern.size();
+        var shortSide = 1;
+        for(int i = 1; i < length-1; i++) {
+            for(int x = 0; x < shortSide; x++) {
+
+            }
+
+            if(shortSide < length/2) shortSide++;
+            else shortSide--;
+        }
+
+        return 0;
     }
 }
 
