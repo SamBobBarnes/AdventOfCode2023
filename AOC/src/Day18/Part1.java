af8af6dc885af7d7f2309b2e75c2101ce8d617a2
SamBobBarnes <samuelrbarnes@outlook.com>
2023-12-28 18:10:25 -0500

    day 18 init

---
+++ AOC/src/Day18/Part1.java
@@ -0,0 +1,14 @@
+package Day18;
+
+import Base.AdventBase;
+import java.util.List;
+
+public class Part1 extends AdventBase {
+    public static int Run(boolean example) {
+        Start(18, 1, example);
+
+        List<String> input = LoadInput(18, example);
+
+        return 0;
+    }
+}
---
+++ AOC/src/Day18/Part2.java
@@ -0,0 +1,14 @@
+package Day18;
+
+import Base.AdventBase;
+import java.util.List;
+
+public class Part2 extends AdventBase {
+    public static int Run(boolean example) {
+        Start(18, 2, example);
+
+        List<String> input = LoadInput(18, example);
+
+        return 0;
+    }
+}
