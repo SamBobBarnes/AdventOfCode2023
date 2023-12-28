cd4a0bee13d1f90cef8a1d548b92ce910633dc68
SamBobBarnes <samuelrbarnes@outlook.com>
2023-12-28 18:10:29 -0500

    day 19 init

---
+++ AOC/src/Day19/Part1.java
@@ -0,0 +1,14 @@
+package Day19;
+
+import Base.AdventBase;
+import java.util.List;
+
+public class Part1 extends AdventBase {
+    public static int Run(boolean example) {
+        Start(19, 1, example);
+
+        List<String> input = LoadInput(19, example);
+
+        return 0;
+    }
+}
--- AOC/tests/MainRunnerTests.java
+++ AOC/tests/MainRunnerTests.java
@@ -1,15 +1,15 @@
-    //region Day18
+    //region Day19
     @Theory
-    public void Day18Part1(@FromDataPoints("example") boolean example) {
+    public void Day19Part1(@FromDataPoints("example") boolean example) {
 
-        var actual = Day18.Part1.Run(example);
+        var actual = Day19.Part1.Run(example);
 
         Assertions.assertEquals(example ? 0:0, actual);
     }
     @Theory
-    public void Day18Part2(@FromDataPoints("example") boolean example) {
+    public void Day19Part2(@FromDataPoints("example") boolean example) {
 
-        var actual = Day18.Part2.Run(example);
+        var actual = Day19.Part2.Run(example);
 
         Assertions.assertEquals(example ? 0:0, actual);
     }
