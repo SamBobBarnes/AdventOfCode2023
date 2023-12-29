27b765caee1e442a68d5db0dba7de1e1e55739aa
SamBobBarnes <samuelrbarnes@outlook.com>
2023-12-29 13:41:58 -0500

    Day 18 Part 1

--- AOC/src/Day18/Part1.java
+++ AOC/src/Day18/Part1.java
@@ -1,7 +1,12 @@
 package Day18;
 
 import Base.AdventBase;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 public class Part1 extends AdventBase {
     public static int Run(boolean example) {
@@ -9,6 +14,88 @@ public class Part1 extends AdventBase {
 
         List<String> input = LoadInput(18, example);
 
-        return 0;
+        List<DigPlan> plans = input.stream().map(x -> new DigPlan(x.split(" ")[0].charAt(0),parseInt(x.split(" ")[1]))).toList();
+
+        var result = FindArea(plans);
+
+        return result;
+    }
+
+    private static int FindArea(List<DigPlan> plans) {
+        var x = 0;
+        var y = 0;
+
+        for(var plan: plans) {
+            plan.SetStart(x,y);
+            for(int i = 0; i < plan.distance; i++) {
+                switch(plan.direction) {
+                    case 'U':
+                        y--;
+                        break;
+                    case 'D':
+                        y++;
+                        break;
+                    case 'L':
+                        x--;
+                        break;
+                    case 'R':
+                        x++;
+                        break;
+                }
+            }
+            plan.SetEnd(x,y);
+        }
+
+        return Shoelace(plans);
+    }
+
+    private static int Shoelace(List<DigPlan> plans) {
+        var temp = new ArrayList<>(plans);
+        Collections.reverse(temp);
+        var result = 0;
+        var perimeter = 0;
+        for(var plan: temp) {
+            var x0 = plan.endX;
+            var y0 = plan.endY;
+            var x1 = plan.startX;
+            var y1 = plan.startY;
+
+            result += (x0 * y1) - (x1*y0);
+            perimeter += plan.length();
+        }
+        return (Math.abs(result)/2 + perimeter/2) + 1;
+    }
+}
+
+class DigPlan {
+    public char direction;
+    public int distance;
+    public int startX;
+    public int startY;
+    public int endX;
+    public int endY;
+    public DigPlan(char direction, int distance) {
+        this.direction = direction;
+        this.distance = distance;
+    }
+
+    public void SetStart(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    public void SetEnd(int x, int y) {
+        this.endX = x;
+        this.endY = y;
+    }
+
+    public int length() {
+        if(startY == endY) return Math.abs(endX-startX);
+        return Math.abs(endY-startY);
+    }
+
+    @Override
+    public String toString() {
+        return this.endX + "," + this.endY;
     }
 }
--- AOC/tests/MainRunnerTests.java
+++ AOC/tests/MainRunnerTests.java
@@ -1,15 +1,308 @@
-    //region Day25
-    @Theory
-    public void Day25Part1(@FromDataPoints("example") boolean example) {
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
 
-        var actual = Day25.Part1.Run(example);
+
+@RunWith(Theories.class)
+public class MainRunnerTests {
+    @DataPoints("example")
+    public static boolean[] example() {
+        return new boolean[]{true, false};
+    }
+
+    //region Day1
+    @Theory
+    public void Day1Part1() {
+
+        int actual = Day1.Part1.Run(false);
+
+        Assertions.assertEquals(53651, actual);
+    }
+
+    @Theory
+    public void Day1Part2(@FromDataPoints("example") boolean example) {
+
+        int actual = Day1.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 281 : 53894, actual);
+    }
+
+    //endregion
+    //region Day2
+    @Theory
+    public void Day2Part1(@FromDataPoints("example") boolean example) {
+
+        int actual = Day2.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 8 : 2207, actual);
+    }
+
+    @Theory
+    public void Day2Part2(@FromDataPoints("example") boolean example) {
+
+        int actual = Day2.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 2286 : 62241, actual);
+    }
+
+    //endregion
+    //region Day3
+    @Theory
+    public void Day3Part1(@FromDataPoints("example") boolean example) {
+
+        int actual = Day3.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 4361 : 544664, actual);
+    }
+
+    @Theory
+    public void Day3Part2(@FromDataPoints("example") boolean example) {
+
+        int actual = Day3.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 467835 : 84495585, actual);
+    }
+    //endregion
+    //region Day4
+    @Theory
+    public void Day4Part1(@FromDataPoints("example") boolean example) {
+
+        int actual = Day4.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 13:25004, actual);
+    }
+    @Theory
+    public void Day4Part2(@FromDataPoints("example") boolean example) {
+
+        int actual = Day4.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 30:14427616, actual);
+    }
+    //endregion
+    //region Day5
+    @Theory
+    public void Day5Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day5.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 35:261668924, actual);
+    }
+    @Theory
+    public void Day5Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day5.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 46:24261545, actual);
+    }
+    //endregion
+    //region Day6
+    @Theory
+    public void Day6Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day6.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 288:281600, actual);
+    }
+    @Theory
+    public void Day6Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day6.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 71503:33875953, actual);
+    }
+    //endregion
+    //region Day7
+    @Theory
+    public void Day7Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day7.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 6440:246424613, actual);
+    }
+    @Theory
+    public void Day7Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day7.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 5905:248256639, actual);
+    }
+    //endregion
+    //region Day8
+    @Theory
+    public void Day8Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day8.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 6:17873, actual);
+    }
+    @Test
+    public void Day8Part2() {
+
+        var actual = Day8.Part2.Run(false);
+
+        Assertions.assertEquals(15746133679061L, actual);
+    }
+    //endregion
+    //region Day9
+    @Theory
+    public void Day9Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day9.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 114:1993300041, actual);
+    }
+    @Theory
+    public void Day9Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day9.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 2:1038, actual);
+    }
+    //endregion
+    //region Day10
+    @Theory
+    public void Day10Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day10.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 80:7093, actual);
+    }
+    @Theory
+    public void Day10Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day10.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 10:0, actual);
+    }
+    //endregion
+    //region Day11
+    @Theory
+    public void Day11Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day11.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 374:9233514, actual);
+    }
+    @Theory
+    public void Day11Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day11.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 8410:363293506944L, actual);
+    }
+    //endregion
+    //region Day12
+    @Theory
+    public void Day12Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day12.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 21:0, actual);
+    }
+    @Theory
+    public void Day12Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day12.Part2.Run(example);
 
         Assertions.assertEquals(example ? 0:0, actual);
     }
+    //endregion
+    //region Day13
     @Theory
-    public void Day25Part2(@FromDataPoints("example") boolean example) {
+    public void Day13Part1(@FromDataPoints("example") boolean example) {
 
-        var actual = Day25.Part2.Run(example);
+        var actual = Day13.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 405:29130, actual);
+    }
+    @Theory
+    public void Day13Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day13.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 400:33438, actual);
+    }
+    //endregion
+    //region Day14
+    @Theory
+    public void Day14Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day14.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 136:110274, actual);
+    }
+    @Theory
+    public void Day14Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day14.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 64:90982, actual);
+    }
+    //endregion
+    //region Day15
+    @Theory
+    public void Day15Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day15.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 1320:521341, actual);
+    }
+    @Theory
+    public void Day15Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day15.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 145:252782, actual);
+    }
+    //endregion
+    //region Day16
+    @Theory
+    public void Day16Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day16.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 46:7415, actual);
+    }
+    @Theory
+    public void Day16Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day16.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 51:0, actual);
+    }
+    //endregion
+    //region Day17
+    @Theory
+    public void Day17Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day17.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 102:0, actual);
+    }
+    @Theory
+    public void Day17Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day17.Part2.Run(example);
+
+        Assertions.assertEquals(example ? 0:0, actual);
+    }
+    //endregion
+    //region Day18
+    @Theory
+    public void Day18Part1(@FromDataPoints("example") boolean example) {
+
+        var actual = Day18.Part1.Run(example);
+
+        Assertions.assertEquals(example ? 62:34329, actual);
+    }
+    @Theory
+    public void Day18Part2(@FromDataPoints("example") boolean example) {
+
+        var actual = Day18.Part2.Run(example);
 
         Assertions.assertEquals(example ? 0:0, actual);
     }
